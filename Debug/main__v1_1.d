cc5af1c00d8280da1f15831f132b07b5d794b0e7
rjibanezalcala <rjibanezalcala@miners.utep.edu>
2022-04-22 15:09:31 -0600

    Add files via upload

---
+++ Debug/main__v1_1.d
@@ -0,0 +1,54 @@
+# FIXED
+
+main__v1_1.obj: ../main__v1_1.c
+main__v1_1.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430.h
+main__v1_1.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430fr2355.h
+main__v1_1.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h
+main__v1_1.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/intrinsics.h
+main__v1_1.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/intrinsics_legacy_undefs.h
+main__v1_1.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/legacy.h
+main__v1_1.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/stdio.h
+main__v1_1.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/_ti_config.h
+main__v1_1.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/linkage.h
+main__v1_1.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/stdarg.h
+main__v1_1.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/sys/_types.h
+main__v1_1.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/sys/cdefs.h
+main__v1_1.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/machine/_types.h
+main__v1_1.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/stdlib.h
+main__v1_1.obj: ../RECORD.h
+main__v1_1.obj: ../arenacfg1.h
+
+../main__v1_1.c:
+
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430fr2355.h:
+
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/intrinsics.h:
+
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/legacy.h:
+
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/stdio.h:
+
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/_ti_config.h:
+
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/linkage.h:
+
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/stdarg.h:
+
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/sys/_types.h:
+
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/machine/_types.h:
+
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.4.LTS/include/stdlib.h:
+
+../RECORD.h:
+
+../arenacfg1.h:
+
---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr2355.cmd 
+
+C_SRCS += \
+../main__v1_1.c 
+
+C_DEPS += \
+./main__v1_1.d 
+
+OBJS += \
+./main__v1_1.obj 
+
+OBJS__QUOTED += \
+"main__v1_1.obj" 
+
+C_DEPS__QUOTED += \
+"main__v1_1.d" 
+
+C_SRCS__QUOTED += \
+"../main__v1_1.c" 
+
+
